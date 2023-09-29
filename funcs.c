0aa791b537238f77fea5d2711b9c9dfff59b672d
João Pedro Bazoli palma <50723180+joaopedroBP@users.noreply.github.com>
2023-09-29 15:17:24 -0300

    Update funcs.c

--- funcs.c
+++ funcs.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 #include "funcs.h"
 
-void menu(){
-    printf("bem vindo ao seu banco \n");
-    printf("1: Cadastrar novo cliente.\n"
-    "2:Deletar conta.\n"
-    "3:Listar clientes.\n");
+int main(){
+    clientes cls;
+
+    while(1){
+        menu();
+        printf("Qual a sua escolha?:\n");
+        scanf("%d", &escolha);
+
+        switch(escolha){
+            case 1:
+            printf("hello");
+                break;
+        }
+        break;
+    }
+    return 0;
 }
