ca8002e8479a3f025a12bcf9f4f1ac7084b01244
João Pedro Bazoli palma <50723180+joaopedroBP@users.noreply.github.com>
2023-09-29 15:17:47 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,15 +2,16 @@
 #include "funcs.h"
 
 int main(){
-
+    clientes cls;
 
     while(1){
         menu();
         printf("Qual a sua escolha?:\n");
         scanf("%d", &escolha);
-        
+
         switch(escolha){
             case 1:
+            printf("hello");
                 break;
         }
         break;
