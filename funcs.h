f15f2c3a37904a85bf9e4110623e8b4d0ecb6b54
João Pedro Bazoli palma <50723180+joaopedroBP@users.noreply.github.com>
2023-09-29 15:18:30 -0300

    Update funcs.h

--- funcs.h
+++ funcs.h
@@ -1,2 +1,24 @@
+typedef struct{
+    char nome[100];
+    char CPF[500];
+    char conta[100];
+    int valor;
+    char senha[100];
+}cliente;
+
+typedef struct{
+    cliente clientes;
+    int qtd;
+}clientes;
+
+typedef struct{
+    char CPF[500];
+    char info[100][300];
+
+}extrato;
+
+
+
+
 int escolha;
 void menu();
