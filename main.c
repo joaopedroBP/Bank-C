c48fa01e30354597e8438cfc2cd0dd209057f54c
João Pedro Bazoli palma <50723180+joaopedroBP@users.noreply.github.com>
2023-10-15 16:23:03 -0300

    Update main.c

--- main.c
+++ main.c
@@ -4,8 +4,14 @@
 
 int main(){
     clientes cls;
-    cls.qtd = 0;
-    while(1){
+    int cod = lerclientes(&cls, "clientes.bin");
+    if(cod == 1){
+        cls.qtd = 0;
+    }
+
+    int escolha;
+    int T = 0;
+    while(T == 0){
         menu();
         printf("Qual a sua escolha?: ");
         scanf("%d", &escolha);
@@ -14,8 +20,17 @@ int main(){
             case 1:
                 newclient(&cls);
                 break;
+            case 8:
+                T++; 
+                break;
         }
-        break;
+    }
+
+    cod = saveclients(&cls,"clientes.bin");
+    if(cod == 1){
+        printf("falha ao salvar dados");
+    }else{
+        printf("Dados salvos com exito, obrgado por usar os nossos serviços");
     }
     return 0;
 }
